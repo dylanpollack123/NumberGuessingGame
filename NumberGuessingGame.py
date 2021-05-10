af28b0a163b3dfbee10e902a035247cb87cf0070
Dylan Pollack <83979919+dylanpollack123@users.noreply.github.com>
2021-05-10 16:10:50 -0400

    NumberGuessingGame Python code
    *If a user wants to run the actual code, they must have the integrated development environment for Python installed, IDLE.*

---
+++ NumberGuessingGame.py
@@ -0,0 +1,45 @@
+#Number Guessing Game
+from tkinter import *
+from tkinter.messagebox import showinfo
+import random
+
+class Application(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.grid()
+        
+        self.create_widgets()
+        self.number = str(random.randrange(0,9))
+        #Creates random number range between 1-9
+    def create_widgets(self):
+        self.lbl1 = Label(self, text = "Number Guessing Game")
+        self.lbl1.grid(row = 0, column = 0)
+        
+        self.entry1 = Entry(self, width = 40)
+        self.entry1.grid(row = 1, columnspan = 5, sticky =W)
+        self.entry1.bind('<KeyPress>', self.enter)
+        #Clicking enter key is the same as clicking the 'Guess' button
+        self.bttn = Button(self, text= "Guess", command = self.number_guess)
+        self.bttn.grid(row = 2, column = 2, sticky =W)
+        
+    def number_guess(self):
+        print(self.number)
+        if self.entry1.get() == self.number:
+            showinfo(message = "Correct")
+        #New seperate window pops up if guess is correct 
+        else:
+            self.entry1.delete(0, END)
+            self.lbl1["text"] = "Try again!"
+            
+    def enter(self, event):
+        if event.keysym == "Return":
+            self.number_guess()
+#main     
+root = Tk()
+root.title("Number Guessing Game")
+root.geometry("500x500")
+root.resizable(width = FALSE, height = FALSE)
+app = Application(root)
+root.mainloop()
+
+
